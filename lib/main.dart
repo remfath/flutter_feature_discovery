2922bbd71fe69624b01afa13414aa233bb2ce411
remfath <remfath@gmail.com>
2018-05-20 16:38:11 +0800

    Add basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,81 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
+        backgroundColor: Colors.green,
+        leading: new IconButton(
+          icon: new Icon(Icons.menu),
+          onPressed: () {},
+        ),
         title: new Text(''),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: new Center(),
+      body: new Content(),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class Content extends StatefulWidget {
+  @override
+  _ContentState createState() => new _ContentState();
+}
+
+class _ContentState extends State<Content> {
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        new Image.network(
+          'https://cdn.cnn.com/cnnnext/dam/assets/150929101049-black-coffee-stock-super-tease.jpg',
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: 200.0,
+        ),
+        new Container(
+          color: Colors.blue,
+          width: double.infinity,
+          padding: const EdgeInsets.all(16.0),
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: new Text(
+                  'Beauty & Lifetime',
+                  style: new TextStyle(
+                    fontSize: 24.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              new Text(
+                'Coffee Shop',
+                style: new TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ],
+          ),
+        ),
+        new Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(16.0),
+          child: new RaisedButton(
+            child: new Text('Go to have a coffe'),
+            onPressed: () {},
+          ),
+        ),
+      ],
     );
   }
 }
